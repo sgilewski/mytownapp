0f544d2716407337cbc456834357caacc3e1cb8d
sgilewski <sgilewski@me.com>
2026-08-23 15:55:13 -0400

    feat: add deployable app previews

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { DM_Sans, Fraunces } from "next/font/google";
 import "./globals.css";
+import "./demo.css";
 import "./forms.css";
 const sans = DM_Sans({ subsets: ["latin"], variable: "--font-sans" });
 const display = Fraunces({ subsets: ["latin"], variable: "--font-display" });
